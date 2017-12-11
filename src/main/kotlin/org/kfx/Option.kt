a27ec88bbb9be4307ec3e83867bfd5cb99345275
dannyjanz <github666>
2017-12-11 16:59:45 -0500

    adding gradle buildfile, gitignore and blank Readme

---
+++ src/main/kotlin/org/kfx/Option.kt
@@ -0,0 +1,68 @@
+@file:Suppress("UNCHECKED_CAST")
+
+package org.kfx
+
+interface Option<T> : Monad<Option<*>, T>, Container<T> {
+
+    companion object {
+        operator fun <T> invoke(maybe: T?): Option<T> {
+            return if (maybe != null) Some(maybe) else None as Option<T>
+        }
+    }
+
+
+    override fun <R> map(transform: (T) -> R): Option<R>
+    override fun <R> flatMap(bind: (T) -> Monad<Option<*>, R>): Option<R>
+
+}
+
+class Some<T>(private val value: T) : Option<T>, Container<T> by StandardFullContainer(value) {
+
+    override infix fun <R> flatMap(bind: (T) -> Monad<Option<*>, R>): Option<R> = value.let(bind) as Option<R>
+    override infix fun <R> map(transform: (T) -> R): Option<R> = value.let(transform).let { Some(it) }
+
+    override fun toString(): String = "Some(${value.toString()})"
+
+    override fun equals(other: Any?): Boolean {
+        return if (other != null) {
+            when (other) {
+                is Some<*> -> other.value!! == value
+                else -> false
+            }
+        } else {
+            false
+        }
+    }
+
+    override fun hashCode(): Int = value!!.hashCode()
+
+}
+
+sealed class None<T> : Option<T>, StandardEmptyContainer<T> {
+
+    override fun <R> flatMap(bind: (T) -> Monad<Option<*>, R>): Option<R> = None as Option<R>
+    override infix fun <R> map(transform: (T) -> R): Option<R> = None as Option<R>
+
+    override fun toString(): String = "None"
+
+    companion object : None<Nothing>() {
+        operator fun <T>invoke(): None<T> = this as None<T>
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return if (other != null) {
+            when (other) {
+                is None<*> -> true
+                else -> false
+            }
+        } else {
+            false
+        }
+    }
+
+    override fun hashCode(): Int = 0
+
+}
+
+fun <T> T?.asOption(): Option<T> = Option(this)
+
