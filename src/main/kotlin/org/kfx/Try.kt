b36c750f697c6e3865f8f9e3e870c7299f6f6cf4
dannyjanz <github666>
2018-02-05 16:11:21 -0500

    Adding Filterable to Try with implementation, as well as recover and recoverWith for transformation of the error in Failure, as well as respective U Tests

--- src/main/kotlin/org/kfx/Try.kt
+++ src/main/kotlin/org/kfx/Try.kt
@@ -2,7 +2,7 @@
 
 package org.kfx
 
-interface Try<T> : Monad<Try<*>, T>, Container<T> {
+interface Try<T> : Monad<Try<*>, T>, Filterable<Try<*>, T>, Container<T> {
 
     companion object {
 
@@ -16,12 +16,20 @@ interface Try<T> : Monad<Try<*>, T>, Container<T> {
     }
 
     fun isSuccess(): Boolean
+    fun isFailure(): Boolean
+
+    fun <R> recover(func: (Throwable) -> R): Try<R>
+    fun <R> recoverWith(func: (Throwable) -> Try<R>): Try<R>
 
     override fun <R> map(transform: (T) -> R): Try<R>
     override fun <R> flatMap(bind: (T) -> Monad<Try<*>, R>): Try<R>
+    override fun filter(pred: (T) -> Boolean): Try<T>
 }
 
 class Success<T>(val value: T) : Try<T>, Container<T> by StandardFullContainer(value) {
+
+
+    override fun <R> map(transform: (T) -> R): Try<R> = Try { value.let(transform) }
 
     override fun <R> flatMap(bind: (T) -> Monad<Try<*>, R>): Try<R> = try {
         value.let(bind)
@@ -29,9 +37,15 @@ class Success<T>(val value: T) : Try<T>, Container<T> by StandardFullContainer(v
         Failure(e)
     } as Try<R>
 
-    override fun <R> map(transform: (T) -> R): Try<R> = Try { value.let(transform) }
+    override fun filter(pred: (T) -> Boolean): Try<T> =
+            if (pred(value)) this
+            else Failure(NoSuchElementException("Predicate does not hold for $value")) as Try<T>
+
+    override fun <R> recover(func: (Throwable) -> R): Try<R> = this as Try<R>
+    override fun <R> recoverWith(func: (Throwable) -> Try<R>): Try<R> = this as Try<R>
 
     override fun isSuccess(): Boolean = true
+    override fun isFailure(): Boolean = false
 
     override fun equals(other: Any?): Boolean = other ifNotNull {
         when (it) {
@@ -41,14 +55,25 @@ class Success<T>(val value: T) : Try<T>, Container<T> by StandardFullContainer(v
     } ?: false
 
     override fun hashCode(): Int = value?.hashCode() ?: 0
+
+    override fun toString(): String = "Success(${value.toString()})"
 }
 
 class Failure<T> private constructor(val error: Throwable) : Try<T>, StandardEmptyContainer<T> {
 
-    override fun <R> flatMap(bind: (T) -> Monad<Try<*>, R>): Try<R> = Failure(error)
-    override fun <R> map(transform: (T) -> R): Try<R> = Failure(error)
+    override fun <R> map(transform: (T) -> R): Try<R> = this as Try<R>
+    override fun <R> flatMap(bind: (T) -> Monad<Try<*>, R>): Try<R> = this as Try<R>
+    override fun filter(pred: (T) -> Boolean): Try<T> = this
 
     override fun isSuccess(): Boolean = false
+    override fun isFailure(): Boolean = true
+
+    override fun <R> recover(func: (Throwable) -> R): Try<R> = Try { func(error) }
+    override fun <R> recoverWith(func: (Throwable) -> Try<R>): Try<R> = try {
+        func(error)
+    } catch (e: Throwable) {
+        Failure(e)
+    }
 
     override fun equals(other: Any?): Boolean = other ifNotNull {
         when (it) {
@@ -59,8 +84,13 @@ class Failure<T> private constructor(val error: Throwable) : Try<T>, StandardEmp
 
     override fun hashCode(): Int = error.hashCode()
 
+    override fun toString(): String = "Failure($error)"
+
     companion object {
         operator fun invoke(error: Throwable): Failure<Nothing> = Failure(error)
     }
 
 }
+
+fun <T> T.asSuccess(): Success<T> = Success(this)
+fun <T> Throwable.asFailure(): Failure<T> = Failure(this) as Failure<T>
--- src/test/kotlin/org/kfx/TryUTest.kt
+++ src/test/kotlin/org/kfx/TryUTest.kt
@@ -1,10 +1,17 @@
+@file:Suppress("UNREACHABLE_CODE")
+
 package org.kfx
 
+import io.kotlintest.matchers.beOfType
+import io.kotlintest.matchers.should
 import io.kotlintest.matchers.shouldBe
 import io.kotlintest.specs.FunSpec
 
 class TryUTest : FunSpec() {
     init {
+
+        val error = Throwable("too bad :(")
+        val otherError = IllegalAccessError("You shouldn't be here..")
 
         test("invoking a Try with a succeeding operation should result in a Success of the result") {
 
@@ -12,8 +19,6 @@ class TryUTest : FunSpec() {
         }
 
         test("invoking a Try with a failing operation should result in a Failure with the occurred exception") {
-
-            val error = Throwable("too bad :(")
 
             Try { throw error } shouldBe Failure(error)
         }
@@ -28,8 +33,6 @@ class TryUTest : FunSpec() {
         }
 
         test("an error occurring in mapping should be caught by the Try and result in a Failure") {
-
-            val error = Throwable("woops..")
 
             Try { 1 } map { throw error } shouldBe Failure(error)
         }
@@ -46,7 +49,6 @@ class TryUTest : FunSpec() {
 
         test("binding a Try to another Try should result in a Failure if any of them is Failure") {
 
-            val error = Throwable("too bad :(")
             val try1 = Try { 1 }
             val try2 = Failure(error)
 
@@ -56,10 +58,43 @@ class TryUTest : FunSpec() {
 
         test("an error occurring in the binding should result in a Failure") {
 
-            val error = Throwable("woops..")
+            Try { 1 } flatMap { throw error; Try { "1" } } shouldBe Failure(error)
 
-            Try { 1 } flatMap { throw error; Try {"1"} } shouldBe Failure(error)
+        }
 
+        test("filtering a Try should result in a Success if the original was a Success and the predicate is satisfied") {
+
+            Try { 2 } filter { it < 3 } shouldBe Success(2)
+        }
+
+        test("filtering a Try should result in a Failure if the predicate does not hold or the original is a Failure") {
+
+            Try { 2 } filter { it == 3 } should beOfType<Failure<*>>()
+            Failure(error) filter { true } shouldBe Failure(error)
+        }
+
+        test("recover should transform the error in a Failure into a new Try") {
+
+            Failure(error).recover { 10 } shouldBe Success(10)
+            Failure(error).recover { throw otherError } shouldBe Failure(otherError)
+        }
+
+        test("recover should return 'this' in case it is a Success") {
+
+            Success(1).recover { 2 } shouldBe Success(1)
+            Success(1).recover { throw error } shouldBe Success(1)
+        }
+
+        test("binding a Try to a Failure via recoverWith should result in a new Try with the transformation of the error") {
+
+            Failure(error).recoverWith { Success(2) } shouldBe Success(2)
+            Failure(error).recoverWith { Failure(otherError) } shouldBe Failure(otherError)
+        }
+
+        test("recoverWith should return 'this' in case it was a Success") {
+
+            Success(1).recoverWith { Success(2) } shouldBe Success(1)
+            Success(1).recoverWith { Failure(error) } shouldBe Success(1)
         }
     }
 }
