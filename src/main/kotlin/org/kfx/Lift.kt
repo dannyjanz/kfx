76e2e9228a2c54fea5a5c2b0441c414e486c1c77
dannyjanz <janz.danny@gmail.com>
2018-02-16 15:01:48 -0500

    adding Lift interface (a little late..)

---
+++ src/main/kotlin/org/kfx/Lift.kt
@@ -0,0 +1,11 @@
+package org.kfx
+
+interface Lift<F> {
+
+    fun <T, R> lift(func: (T) -> R): (Functor<F, T>) -> Functor<F, R> = { a: Functor<F, T> -> a.map(func) }
+
+}
+
+
+fun <T, R, F : Functor<F, *>> Functor<F, T>.lift(func: (T) -> R): (Functor<F, T>) -> Functor<F, R> = { a: Functor<F, T> -> a.map(func) }
+
