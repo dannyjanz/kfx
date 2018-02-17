e0b1362ca49866126bc3a1b36d1bce56f52d4363
dannyjanz <janz.danny@gmail.com>
2018-02-16 23:33:13 -0500

    adding interface TraverseWith as typeclass for mapping over sequence with a function producing a monad and lifting the result in one step.

---
+++ src/main/kotlin/org/kfx/Traverse.kt
@@ -0,0 +1,10 @@
+package org.kfx
+
+interface TraverseWith<M> {
+
+    fun <T, R> traverse(list: List<T>, function: (T) -> Monad<M, R>): Monad<M, List<R>>
+
+}
+
+fun <T, R, M : Monad<*, *>> List<T>.traverse(traverseWith: TraverseWith<M>, function: (T) -> Monad<M, R>) =
+        traverseWith.traverse(this, function)
