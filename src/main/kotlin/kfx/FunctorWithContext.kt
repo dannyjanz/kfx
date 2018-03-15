23945ca22f606f2b2a7dee9843a2dcac4df6640a
dannyjanz <janz.danny@gmail.com>
2018-03-15 15:56:31 -0400

    fixing return type on functor with context

--- src/main/kotlin/kfx/FunctorWithContext.kt
+++ src/main/kotlin/kfx/FunctorWithContext.kt
@@ -2,6 +2,6 @@ package kfx
 
 interface FunctorWithContext<Self, out T, in C> : Functor<Self, T> {
 
-    fun <R> map(context: C, transform: (T) -> R): Functor<Self, R>
+    fun <R> map(context: C, transform: (T) -> R): FunctorWithContext<Self, R, C>
 
 }
